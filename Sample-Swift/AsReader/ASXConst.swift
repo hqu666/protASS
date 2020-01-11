eaee7477262ed41b3962a18557268345d897f440
桑山博臣 <hkuwayama@coresoft-net.co.jp>
2020-01-11 12:28:36 +0900

    0

---
+++ Sample-Swift/AsReader/ASXConst.swift
@@ -0,0 +1,16 @@
+//
+//  ASXConst.swift
+//  Sample
+//
+//
+
+import Foundation
+
+
+struct ASXConst {
+    static let PREFIX_ASREADER_PROTOCOLS =      "jp.co.asx.asreader."
+    static let PROTOCOL_BARCODE =               "jp.co.asx.asreader.barcode"
+    static let PROTOCOL_RFID =                  "jp.co.asx.asreader.rfid"
+    static let PROTOCOL_DONGLE_BARCODE =        "jp.co.asx.asreader.6dongle.barcode"
+    static let PROTOCOL_DONGLE_RFID =           "jp.co.asx.asreader.6dongle.rfid"
+}
